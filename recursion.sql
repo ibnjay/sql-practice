a5d61d297a6ad1a82777704e21e7e8f4213b589e
Dhananjay Tripathi <ibnjay@gmail.com>
2022-06-30 01:28:03 -0400

    another recursion example

--- recursion.sql
+++ recursion.sql
@@ -8,3 +8,18 @@ from numbers where n < 10
 )
 
 select * from numbers ;
+
+--  find the hierachy of employee under a given manger.
+
+with recursive emp_hierachy as
+    ( select id, name, manager_id, designation, 1 as lvl
+    from emp_details where name='Asha'
+    union
+    select E.id, E.name, E.manager_id, E.designation, H.lvl+1 as lvl
+    from emp_hierachy 
+    join emp_details E on H.id = E.manager_id
+    )
+select * from emp_hierachy ;
+
+
+--  find the hierachy of managers  for a given employee.
